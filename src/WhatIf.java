dd0052dc124d19bf40d00e7ec41a30fc64267c33
greendb16 <greendb16@gmail.com>
2019-06-13 15:28:01 -0400

    Final Final Commit

--- src/WhatIf.java
+++ src/WhatIf.java
@@ -3,36 +3,36 @@ import java.util.Scanner;
 
 public class WhatIf {
     public static void main(String[] args) {
-        int num1, num2;
+        double num1, num2;
 
         Scanner key = new Scanner(System.in);
 
 
         while (true) {
             System.out.println("First number? : ");
-            num1 = key.nextInt();
+            num1 = key.nextDouble();
 
             System.out.println("Second Number? : ");
-            num2 = key.nextInt();
+            num2 = key.nextDouble();
             key.nextLine();
 
-            int sum = (num1 + num2);
-            int product = (num1 * num2);
-            int average = ((num1 + num2) / 2);
+            double sum = (num1 + num2);
+            double product = (num1 * num2);
+            double average = ((num1 + num2) / 2);
 
             if (sum < 1000 && sum > 200) {
-                System.out.println("Sum is ~" + sum + "*");
+                System.out.printf("Sum is ~%.02f*\n", sum); //+ sum + "*");
             } else if (sum < 1000) {
-                System.out.println("Sum is ~" + sum);
+                System.out.printf("Sum is ~%.02f\n", sum);
             } else {
-                System.out.println("Sum is " + sum + "*");
+                System.out.printf("Sum is %.02f*\n", sum);
             }
-            System.out.println("Product is " + product);
-            System.out.println("Average is " + average);
-            System.out.println("Would you like to go again ?");
+            System.out.printf("Product is %.02f\n", product);
+            System.out.printf("Average is %.02f\n", average );
+            System.out.println("\nWould you like to go again ?");
             String answer = key.nextLine();
             if (answer.equalsIgnoreCase("yes")){
-                System.out.println("Hooray!!!!");}
+                System.out.println("Hooray!!!!\n");}
                 else{
                 System.out.println("Goodbye")
                     ;break;}
